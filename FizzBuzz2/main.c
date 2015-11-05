8720e6f193538bc314fc1ea65b286c15d7e6c53c
Kerry Toonen <kerrytoonen@neva-bassingthwaites-macbook-pro.local>
2015-11-05 22:15:20 +0900

    Here is a better version of FizzBuzz

--- FizzBuzz2/main.c
+++ FizzBuzz2/main.c
@@ -6,10 +6,24 @@
 //  Copyright © 2015 Kerrto. All rights reserved.
 //
 
-#include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+# include <stdio.h>
+int main () {
+    int i;
+    for (i=1 ; i<=100 ; i++)
+        
+        if (i%3==0)
+            
+            printf("Fizz\n");
+    
+        else if (i%5==0)
+            
+            printf ("Buzz\n");
+    
+        else
+            
+            printf ("%d\n",i);
+    
     return 0;
+    
 }
